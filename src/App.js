0764b263fc78e51d69de74fe004917b727bdf510
lightofdavinci <myrovoy@mail.ru>
2016-10-20 14:51:18 -0400

    'replacing-social-buttons'

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import { articles } from './articles';
 import React, { Component } from 'react';
-import './App.css';
 import jumbo from './panda-head.jpg';
 import ImageList from './ImageList';
-import { articles } from './articles';
+import SocialButtons from './SocialButtons';
+import './App.css';
+
 
 export default class App extends Component {
   render() {
@@ -14,14 +16,7 @@ export default class App extends Component {
         <div className="App-main-nav"></div>
         <div className="App-nav"></div>
         <ImageList articles={articles} />
-        <div className="App-social-buttons">
-          <ul className="social__nav">
-            <li><a href="https://www.facebook.com/dan.volosnikov"><i className="fa fa-facebook fa-lg"></i></a></li>
-            <li><a href="http://codepen.io/Volosnikov/"><i className="fa fa-codepen fa-lg"></i></a></li>
-            <li><a href="https://github.com/lightofdavinci"><i className="fa fa-github fa-lg"></i></a></li>
-            <li><a href="https://vk.com/danymccoyvolosnikov"><i className="fa fa-vk fa-lg"></i></a></li>
-          </ul>
-        </div>
+        <SocialButtons />
         <div className="App-footer"></div>
       </div>
     );
---
+++ src/SocialButtons.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './SocialButtons.css';
+
+export default () => (
+  <div className="App-social-buttons">
+    <ul className="social__nav">
+      <li><a href="https://www.facebook.com/dan.volosnikov"><i className="fa fa-facebook fa-lg"></i></a></li>
+      <li><a href="http://codepen.io/Volosnikov/"><i className="fa fa-codepen fa-lg"></i></a></li>
+      <li><a href="https://github.com/lightofdavinci"><i className="fa fa-github fa-lg"></i></a></li>
+      <li><a href="https://vk.com/danymccoyvolosnikov"><i className="fa fa-vk fa-lg"></i></a></li>
+    </ul>
+  </div>
+);
