ea573c859a1f02a4dc662a2d3b479016ace3db2b
lightofdavinci <myrovoy@mail.ru>
2016-11-29 19:07:30 -0500

    dropzone error handling

--- src/AddPostForm.jsx
+++ src/AddPostForm.jsx
@@ -5,14 +5,16 @@ import Progress from 'react-progressbar';
 import './css/AddPostForm.css';
 
 export default class AddPostForm extends Component {
-  constructor(props) {
-   super(props);
+  constructor() {
+   super();
    this.state = {
       file: {},
       preview: "",
       title: "",
       description: "",
-      progress: ""
+      progress: "",
+      dropZoneMsg: "",
+      dropZoneErr: false
    };
    this.dropzoneOptions = {
       multiple: false,
@@ -27,16 +29,20 @@ export default class AddPostForm extends Component {
   onDrop(accepted, rejected){
     if(accepted[0] !== undefined){
       const file = accepted[0];
-      document.getElementsByClassName('drop__zone__error')[0].style.display = "none";
       document.getElementsByClassName('drop__zone__preview')[0].style.display = "block";
       this.setState({
         preview: file.preview,
-        file: file
+        file: file,
+        dropZoneErr: false,
+        dropZoneMsg: ""
        });
       document.getElementsByClassName('drop__zone__paragraph')[0].style.display = "none";
     }
     if(rejected[0] !== undefined){
-      document.getElementsByClassName('drop__zone__error')[0].style.display = "block";
+      this.setState({
+        dropZoneErr: true,
+        dropZoneMsg: "Only jpg, gif, and png images with a size of 0.03 - 5mb are allowed"
+      });
     }
   };
   handleTitleChange(event) {
@@ -47,6 +53,8 @@ export default class AddPostForm extends Component {
   };
   handleSubmit(event){
     event.preventDefault();
+    if(this.state.title && this.state.description && !(Object.keys(this.state.file).length === 0 && this.state.file.constructor === Object)){
+    this.setState({dropZoneMsg: "", dropZoneErr: false});
     const s4 = () => {
       return Math.floor((1 + Math.random()) * 0x10000)
         .toString(16)
@@ -56,13 +64,11 @@ export default class AddPostForm extends Component {
       return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
         s4() + '-' + s4() + s4() + s4();
     };
-    const storage = firebase.storage();
-    const storageRef = storage.ref();
-    const imagesRef = storageRef.child("images/" + guid());
+    const imagesRef = firebase.storage().ref().child("images/" + guid());
     const uploadTask = imagesRef.put(this.state.file);
     uploadTask.on("state_changed",(snapshot)=>{
-          let percent = snapshot.bytesTransferred / snapshot.totalBytes * 100;
-          this.setState({ progress: percent });
+          let progress = snapshot.bytesTransferred / snapshot.totalBytes * 100;
+          this.setState({ progress });
        });
     uploadTask.then((snapshot)=>{
        firebase.database().ref('articles').push({
@@ -73,10 +79,16 @@ export default class AddPostForm extends Component {
              }).then(()=>{
                document.getElementsByClassName("drop__zone__preview")[0].style.display = "none";
                document.getElementsByClassName("drop__zone__paragraph")[0].style.display = "block";
-               this.setState({preview: "", title: "", description: "", file: {}, progress: "" });
+               this.setState({preview: "", title: "", description: "", file: {}, progress: ""});
                document.getElementsByClassName("add__post__modal")[0].style.display = "none";
              });
     });
+    } else {
+      this.setState({
+        dropZoneErr: true,
+        dropZoneMsg: "Title, description, and image are required for each article"
+      });
+    }
   };
   render(){
     return (
@@ -84,7 +96,9 @@ export default class AddPostForm extends Component {
         <form className="add__post__form" onSubmit={this.handleSubmit}>
           <input className="add__post__title" type="text" placeholder="Post title..." value={this.state.title}  onChange={this.handleTitleChange}/><br/>
           <textarea className="add__post__description" placeholder="Tell us your story here..." value={this.state.description}  onChange={this.handleDescriptionChange}></textarea><br/>
-          <div className="drop__zone__error"><p>Only jpg, gif, and png images with a size of 0.03 - 5mb are allowed </p></div>
+          <div className={(this.state.dropZoneErr)?"drop__zone__error":""}>
+            <p>{this.state.dropZoneMsg}</p>
+          </div>
           <Dropzone className="drop__zone" activeClassName="active__zone"  onDrop={this.onDrop.bind(this)} {...this.dropzoneOptions}>
               <p className="drop__zone__paragraph">Drop image file here, or click to select image to upload.<br/>30kb &#60; jpg, gif, and png images &#60; 5mb </p>
               <img className="drop__zone__preview" src={this.state.preview} alt="preview"/>
